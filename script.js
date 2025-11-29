90e20c7019ee1969d387979e1c56b5e7e91d430e
Rakib-Ahmed2020 <64997553+Rakib-Ahmed2020@users.noreply.github.com>
2025-11-29 06:33:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -46,12 +46,12 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 'lineman1', name: 'Harunur Rashid', designation: 'Line Technician', photo: './harun.png', shortName: 'Harun', bloodGroup: 'B+', mobileNumber: '01708658384' },
         { id: 'lineman2', name: 'Md. Habibur Rahman', designation: 'Lineman Grade-1', photo: './20250716_195749.jpg', shortName: 'Habib', bloodGroup: 'B+', mobileNumber: '01721514164' },
         { id: 'lineman3', name: 'Shahinur Rahman', designation: 'Lineman Grade-1', photo: './shahinurlm.jpg', shortName: 'Shahinur', bloodGroup: 'AB+', mobileNumber: '01721338529' },
-        { id: 'lineman4', name: 'Md. Iqram Hossain', designation: 'Lineman Grade-2', photo: './1000078061.jpg', shortName: 'Iqram', bloodGroup: 'AB+', mobileNumber: '01856329315' },
+        { id: 'lineman4', name: 'Md. Iqram Hossain', designation: 'Lineman Grade-2', photo: './iqram.jpg', shortName: 'Iqram', bloodGroup: 'AB+', mobileNumber: '01856329315' },
         { id: 'lineman5', name: 'Md. Khairul Islam', designation: 'Lineman Grade-2 ', photo: './1000078055.jpg', shortName: 'Khairul', bloodGroup: 'AB+', mobileNumber: '01963518200' },
-        { id: 'lineman6', name: 'Md. Rakib Ahmed', designation: 'Lineman Grade-2', photo: './1000078060.jpg', shortName: 'Rakib', bloodGroup: 'O+', mobileNumber: '01735298588' },
+        { id: 'lineman6', name: 'Md. Rakib Ahmed', designation: 'Lineman Grade-2', photo: './Rakib_profile.jpg', shortName: 'Rakib', bloodGroup: 'O+', mobileNumber: '01735298588' },
         { id: 'lineman7', name: 'Mahim Hossain', designation: 'AP LM', photo: './mahim.jpg', shortName: 'Mahim', bloodGroup: 'O+', mobileNumber: '01770885997' },
         { id: 'lineman8', name: 'Md. Samsul Haque', designation: 'L/S', photo: './1000078056.jpg', shortName: 'Samsu', bloodGroup: 'O+', mobileNumber: '01717973288' },
-        { id: 'lineman9', name: 'Md. Rafiqul Islam', designation: 'L/S', photo: './IMG-20250717-WA0002.jpg', shortName: 'Rafiq', bloodGroup: 'A+', mobileNumber: '01918039245' },
+        { id: 'lineman9', name: 'Md. Rafiqul Islam', designation: 'L/S', photo: './rofik.png', shortName: 'Rafiq', bloodGroup: 'A+', mobileNumber: '01918039245' },
 
     ];
 
@@ -100,8 +100,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const rotationPatterns = [
         // Week 1 Pattern: 1st employee Sat, 2nd Sun, 3rd Mon, 4th Tue, 5th Wed, 6th Thu, 7th Fri
         {
-            6: ['lineman3', 'lineman4'], // Saturday
-            0: ['lineman5'], // Sunday
+            6: ['lineman3', 'lineman4', 'lineman5'], // Saturday
+            0: [], // Sunday
             1: ['lineman6'], // Monday
             2: ['lineman7'], // Tuesday
             3: ['lineman8'], // Wednesday
@@ -110,10 +110,10 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         // Week 2 Pattern: 1st Sun, 2nd Mon, 3rd & 7th Sat, 4th Wed, 5th Thu, 6th Tue
         {
-            0: ['lineman7'], // Sunday
+            0: [], // Sunday
             1: ['lineman1'], // Monday
             5: ['lineman3','lineman4'], // Friday
-            6: ['lineman5', 'lineman6'], // Saturday
+            6: ['lineman5', 'lineman6','lineman7'], // Saturday
             3: ['lineman9'], // Wednesday
             4: ['lineman8'], // Thursday
             2: ['lineman2'] // Tuesday
@@ -125,12 +125,12 @@ document.addEventListener('DOMContentLoaded', () => {
             2: ['lineman4'], // Tuesday
             3: ['lineman2'], // Wednesday
             4: ['lineman9'], // Thursday
-            5: ['lineman5', 'lineman6'], // Friday
-            6: ['lineman7', 'lineman8'], // Saturday (Two employees)
+            5: ['lineman5', 'lineman6','lineman7'], // Friday
+            6: ['lineman7'], // Saturday (Two employees)
             0: ['lineman1'] // Sunday
             // Note: No one is explicitly listed for Friday in Week 3, based on your description.
         },
-        // Week 4 Pattern: 1st Tue, 2nd Wed, 3rd Thu, 4th Sat, 5th Sun, 6th Fri, 7th Mon
+        
         {
             2: ['lineman5'], // Tuesday
             3: ['lineman6','lineman1'], // Wednesday
